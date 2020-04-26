d604f190e29314ce258a0618557e845fd38f8f03
Unknown <j.cerny.zdar@gmail.com>
2020-04-26 13:23:18 +0200

    add latex macros support

--- _plugins/katex.rb
+++ _plugins/katex.rb
@@ -4,19 +4,24 @@ PATH_TO_JS = "./assets/katex/katex.min.js"
 katex_modul = ExecJS.compile(open(PATH_TO_JS).read)
 count = 0
 
+global_macros = Hash.new
+
+for macro_definition in Jekyll.configuration({})['latex-macros']
+	global_macros[macro_definition[0]] = macro_definition[1]
+end
+
 Jekyll::Hooks.register :documents, :post_render do |doc, payload |
 	if doc.data["latex"]
 		rendered_content = doc.output
 		rendered_content = rendered_content.gsub("% <![CDATA[\n", "")
 		rendered_content = rendered_content.gsub("%]]>", "")
 
-
 		display_latex = rendered_content.scan(Regexp.new(/<script type="math\/tex; mode=display">(.*?)<\/script>/)).flatten
 		count += display_latex.size
 		for dl in display_latex
 			rendered_content = rendered_content.gsub('<script type="math/tex; mode=display">' + dl + '</script>',
 				'<div class="equation" style="font-size: 130%">' +
-				katex_modul.call("katex.renderToString", dl, {displayMode: true}) +
+				katex_modul.call("katex.renderToString", dl, {displayMode: true, macros: global_macros}) +
 				'</div>')
 		end
 
@@ -25,7 +30,7 @@ Jekyll::Hooks.register :documents, :post_render do |doc, payload |
 		for il in inline_latex
 			rendered_content = rendered_content.gsub('<script type="math/tex">' + il + '</script>',
 			'<span class="inline-equation">' +
-			katex_modul.call("katex.renderToString", il, {displayMode: false}) +
+			katex_modul.call("katex.renderToString", il, {displayMode: false, macros: global_macros}) +
 			'</span>')
 		end
 
