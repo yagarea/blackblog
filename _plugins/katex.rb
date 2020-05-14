f311ea261d809e897c9217cfb2469d3de209cb84
Unknown <j.cerny.zdar@gmail.com>
2020-05-14 22:05:24 +0200

    bugfix katex.rb

--- _plugins/katex.rb
+++ _plugins/katex.rb
@@ -7,8 +7,10 @@ count = 0
 global_macros = Hash.new
 
 Jekyll::Hooks.register :site, :after_init do |site|
-	for macro_definition in site.config["latex-macros"]
-		global_macros[macro_definition[0]] = macro_definition[1]
+	if site.config["latex-macros"] != nil
+		for macro_definition in site.config["latex-macros"]
+			global_macros[macro_definition[0]] = macro_definition[1]
+		end
 	end
 	print("             LaTeX: " + global_macros.size.to_s + " macros loaded\n")
 end
@@ -19,7 +21,7 @@ Jekyll::Hooks.register :documents, :post_render do |doc, payload|
 		rendered_content = rendered_content.gsub("% <![CDATA[\n", "")
 		rendered_content = rendered_content.gsub("%]]>", "")
 
-		display_latex = rendered_content.scan(Regexp.new(/<script type="math\/tex; mode=display">(.*?)<\/script>/)).flatten
+		display_latex = rendered_content.scan(Regexp.new(/<script type="math\/tex; mode=display">(.*?)<\/script>/m)).flatten
 		count += display_latex.size
 		for dl in display_latex
 			rendered_content = rendered_content.gsub('<script type="math/tex; mode=display">' + dl + '</script>',
@@ -28,7 +30,7 @@ Jekyll::Hooks.register :documents, :post_render do |doc, payload|
 				'</div>')
 		end
 
-		inline_latex = rendered_content.scan(Regexp.new(/<script type="math\/tex">(.*?)<\/script>/)).flatten
+		inline_latex = rendered_content.scan(Regexp.new(/<script type="math\/tex">(.*?)<\/script>/m)).flatten
 		count += inline_latex.size
 		for il in inline_latex
 			rendered_content = rendered_content.gsub('<script type="math/tex">' + il + '</script>',
