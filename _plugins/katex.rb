780c30f679308e33b067baf05c251f5da928f2a4
Jan Černý <j.cerny.zdar@gmail.com>
2021-12-06 20:26:57 +0100

    add dynamic output to katex rendering (closes #13)

--- _plugins/katex.rb
+++ _plugins/katex.rb
@@ -11,7 +11,7 @@ $count_newly_generated_expressions = 0
 $count_cached_expressions = 0
 
 def convert(doc)
-  # Convert HTML enetities back to characters
+  # convert HTML enetities back to characters
   post = HTMLEntities.new.decode(doc.to_s)
   post = post.gsub(/(\\\()((.|\n)*?)(?<!\\)\\\)/) { |m| escape_method($1, $2, doc.path) }
   post = post.gsub(/(\\\[)((.|\n)*?)(?<!\\)\\\]/) { |m| escape_method($1, $2, doc.path) }
@@ -21,7 +21,7 @@ end
 def escape_method( type, string, doc_path )
   @display = false
 
-  # Detect if expression is display view
+  # detect if expression is display view
   case type.downcase
     when /\(/
       @display = false
@@ -36,6 +36,7 @@ def escape_method( type, string, doc_path )
   # use it if it exists
   if(File.exist?(@cache_path))
     $count_cached_expressions += 1
+    print_stats
     return File.read(@cache_path)
 
   # else generate one and store it
@@ -48,6 +49,9 @@ def escape_method( type, string, doc_path )
       # render using ExecJS
       @result =  KATEX.call("katex.renderToString", string, 
                           {displayMode: @display,  macros: $global_macros})
+    rescue SystemExit, Interrupt
+      # this stops jekyll being immune to interupts and kill command
+      raise
     rescue Exception => e
       # Catch parse error
       puts "\e[31m " + e.message.gsub("ParseError: ", "") + "\n\t"  + doc_path + "\e[0m"
@@ -57,8 +61,17 @@ def escape_method( type, string, doc_path )
     File.open(@cache_path, 'w') { |file| file.write(@result) }
     # update count of newly generated expressions
     $count_newly_generated_expressions += 1
+    print_stats
     return @result
   end
+end
+
+def print_stats
+  print "             LaTeX: " + 
+        ($count_newly_generated_expressions + $count_cached_expressions).to_s + 
+        " expressions rendered (" + $count_cached_expressions.to_s + 
+        " already cached)        \r"
+  $stdout.flush
 end
 
 Jekyll::Hooks.register :documents, :post_render do |doc|
@@ -66,13 +79,13 @@ Jekyll::Hooks.register :documents, :post_render do |doc|
 end
 
 Jekyll::Hooks.register :site, :after_init do |site|
-  # Load macros from config file
+  # load macros from config file
   if site.config["latex-macros"] != nil
     for macro_definition in site.config["latex-macros"]
       $global_macros[macro_definition[0]] = macro_definition[1]
     end
   end
-  # Print macro informaion
+  # print macro informaion
   if $global_macros.size == 0
     puts "             LaTeX: no macros loaded"
   else
@@ -82,16 +95,13 @@ Jekyll::Hooks.register :site, :after_init do |site|
 end
 
 Jekyll::Hooks.register :site, :after_reset do
-  # Reset counts after reset
+  # reset counts after reset
   $count_newly_generated_expressions = 0
   $count_cached_expressions = 0
 end
 
 Jekyll::Hooks.register :site, :post_write do
-  # Print stats
-  puts "             LaTeX: " + 
-        ($count_newly_generated_expressions + $count_cached_expressions).to_s + 
-        " expressions rendered (" + $count_cached_expressions.to_s + 
-        " already cached)"
+  # print new line to prevent overwriting previous output
+  print "\n"
 end
 
