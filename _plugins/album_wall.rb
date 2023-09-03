c3ce5e860c2496f822c8084a47e9c4258f1b7e49
Jan Černý <jc@ucw.cz>
2023-09-04 00:50:07 +0200

    implement album wall plugin

---
+++ _plugins/album_wall.rb
@@ -0,0 +1,32 @@
+require 'active_support/core_ext/string/inflections'
+
+module Jekyll
+
+  class AlbumWallBlock < Liquid::Block
+    def render(context)
+      text = super
+      parsed_text = text.split("\n")
+      output = ""
+      for album_line in parsed_text
+        album_line.strip!
+        if album_line == ""
+          next
+        end
+        album = album_line.split("|")
+        album.map { |string| string.strip! }
+
+        band = album[0]
+        title = album[1]
+        cover = band.downcase.parameterize + "-" + title.downcase.parameterize + ".jpg"
+        year = album[2] || "????"
+        link = album[3] || "#"
+        tags = album[4] || ""
+
+        output += %Q[<div class="album-display"><a href="#{link}"><img src="/assets/img/albums/#{cover}" alt="#{band} - #{title}"></a></div>]
+      end
+      return %Q[<div class="album-wall">] + output + "</div>"
+    end
+  end
+end
+
+Liquid::Template.register_tag("album_wall", Jekyll::AlbumWallBlock)
