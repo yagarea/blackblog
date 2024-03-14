9c2c5d04678589bf5db35074a5d0ebdd7ddcdbcd
Jan Černý <jc@ucw.cz>
2024-03-14 14:24:48 +0100

    add details tag

--- _plugins/formater.rb
+++ _plugins/formater.rb
@@ -19,4 +19,30 @@ module Jekyll
   end
 end
 
+module Jekyll
+
+  class Details < Liquid::Block
+
+    def initialize(tag_name, summary, tokens)
+      super
+      @summary = summary.to_s.gsub(/^("|')|("|')$/, '').strip
+    end
+
+    def render(context)
+      @md_converter = context.registers[:site].find_converter_instance(::Jekyll::Converters::Markdown)
+      @content = super.to_s
+      %Q[<details>
+          <summary>
+            #{@md_converter.convert(@summary).to_s.gsub(/^<p>/, '').gsub(/<\/p>$/, '')}
+          </summary>
+          #{@md_converter.convert(@content)}
+        </details>
+        ]
+    end
+  end
+
+end
+
 Liquid::Template.register_tag("spliter", Jekyll::SpliterBlock)
+Liquid::Template.register_tag("details", Jekyll::Details)
+
